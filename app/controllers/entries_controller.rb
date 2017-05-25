9c7063e7a9ef83e3e7369ea9a301ae673b27f0a5
Alex <falsealarm90@gmail.com>
2017-05-25 09:48:56 +0300

    Styling

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,5 @@
 class EntriesController < ApplicationController
+  before_action :load_entry, only: [:destroy]
   respond_to :json, only: [:index]
 
   def index
@@ -23,12 +24,23 @@ class EntriesController < ApplicationController
     respond_with '', location: -> { entries_path }
   end
 
+  def destroy
+    respond_with(@entry.destroy)
+  end
+
+
   def extend
     params["documents"].each do |document_id|
       entry = Entry.where(document_id: document_id, employee_id: params[:employee_id]).first
       entry.update!(expired_at: (entry.expired_at + 1.month))
     end
     respond_with '', location: -> { entries_path }
+  end
+
+  private
+
+  def load_entry
+    @entry = Entry.find(params[:id])
   end
 
 end
