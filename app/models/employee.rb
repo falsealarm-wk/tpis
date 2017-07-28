bf38488c6d8cbfc58dedec666b01d8660f24bc7f
Alex <falsealarm90@gmail.com>
2017-07-28 15:14:42 +0300

    Fixing open entries scope

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ApplicationRecord
   has_many :entries, dependent: :nullify
   has_many :expired_documents, -> { includes(:document).where(closed: false).where('entries.expired_at < ?',Time.zone.now) }, class_name: 'Entry'
-  has_many :open_entries, -> { includes(:document, :employee).where(closed: false, request_id: nil) }, class_name: 'Entry'
+  has_many :open_entries, -> { includes(:document, :employee, :request).where(closed: false, requests: {closed: true}) }, class_name: 'Entry'
 
   scope :with_expired_documents, -> { joins(:entries).where('entries.expired_at < ?',Time.zone.now).uniq }
   default_scope { order(:name)}
